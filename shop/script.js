f2bb367983b83ae34c137f064d98300741756dc2
z60069486-maker <z60069486@gmail.com>
2026-09-23 18:04:22 +0300

    Update script.js

--- shop/script.js
+++ shop/script.js
@@ -42,3 +42,8 @@ buyButton.addEventListener("click", function () {
     message.textContent = "Покупка успешно совершена!";
 
 });
+const backButton = document.getElementById("backButton");
+
+backButton.addEventListener("click", () => {
+    window.location.href = "../menu/index.html";
+});
